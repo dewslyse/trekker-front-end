cbb1415e203f7fde723e14847cb1b1da56273985
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-17 20:35:22 +0300

    Add a secure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Destinations from './pages/Destinations';
+import { fetchDestinations } from './store/actions/destinationActions';
+import RequireAuth from './components/RequireAuth';
 import Home from './pages/Home';
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDestinations());
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {/* Public routes */}
+          <Route path="destinations" element={<Destinations />} />
+          {/* Protected routes */}
+          <Route element={<RequireAuth />}>
+            <Route path="destinations/:id" element={<div>Destination</div>} />
+            <Route path="destinations/:id/reservations" element={<div>Reservations</div>} />
+            <Route path="/*" element={<Destinations />} />
+          </Route>
+
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
+import Notification from '../components/Notification';
+import { hideNotification } from '../store/reducers/uiReducers';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  const destinations = useSelector((state) => state.destinations);
 
-export default Home
+  useEffect(() => {
+    if (destinations.length) {
+      dispatch(hideNotification());
+    }
+  }, [notification]);
+
+  return (
+    <>
+      {notification && (
+      <Notification
+        title={notification.title}
+        message={notification.message}
+      />
+      )}
+      <div>Sidebar</div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Home;
