dfc6adc4d3b7fe3e180c846260f6dc60434a805a
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-19 00:17:00 +0300

    Use deployed api

--- src/api/apiRequests.js
+++ src/api/apiRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://mv-trekker-api.herokuapp.com/api/v1',
   headers: { 'Content-Type': 'application/json' },
 });
 
