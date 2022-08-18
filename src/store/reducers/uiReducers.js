7829c1595b0d2db0a313d0c5ea1224c568d2e85a
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-19 00:18:06 +0300

    Modify ui reducer

--- src/store/reducers/uiReducers.js
+++ src/store/reducers/uiReducers.js
@@ -1,26 +1,41 @@
+/* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isSelected: false,
+  login: false,
+  signup: false,
+  notification: {
+    message: '',
+    isError: false,
+  },
+};
 
 const uiSlice = createSlice({
   name: 'ui',
-  initialState: { visiblity: false, notification: null },
+  initialState,
   reducers: {
-    showNotification: (state, { payload }) => (
-      {
-        visiblity: state.visibility,
-        notification: {
-          status: payload.status,
-          title: payload.title,
-          message: payload.message,
-        },
-      }
-    ),
-    hideNotification: (state) => (
-      { visiblity: state.visibility, notification: null }
-    ),
+    showNotification: (state, { payload }) => {
+      state.notification = payload;
+    },
+    showLogin: (state) => {
+      state.login = true;
+    },
+    hideLogin: (state) => {
+      state.login = false;
+    },
+    showSignup: (state) => {
+      state.signup = false;
+    },
+    hideSignup: (state) => {
+      state.signup = false;
+    },
   },
 });
 
 const { actions, reducer } = uiSlice;
 
-export const { showNotification, hideNotification } = actions;
+export const {
+  showNotification, hideNotification, showLogin, showSignup, hideLogin, hideSignup,
+} = actions;
 export default reducer;
