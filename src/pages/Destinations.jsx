991fcdc213659b3cd55a3befd88ddaa358605a1e
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-08-24 21:43:22 +0300

    Add styles for destinations

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -13,19 +13,19 @@ import Item from './Item';
 
 const responsive = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 1200, min: 768 },
     items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    // slidesToSlide: 3, // optional, default to 1.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 3,
-    slidesToSlide: 3, // optional, default to 1.
+    breakpoint: { max: 768, min: 464 },
+    items: 2,
+    // slidesToSlide: 3, // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1, // optional, default to 1.
+    // slidesToSlide: 1, // optional, default to 1.
   },
 };
 
@@ -36,7 +36,7 @@ const Destinations = () => {
       <h1 className="heading title">LATEST SAFARIS</h1>
       <p className="sub title">Have a funtastic experience</p>
       <p className="sub1 title">```````````````````````</p>
-      <Carousel responsive={responsive} className="carousel" containerClass="carousel-container">
+      <Carousel responsive={responsive} className="carousel" infinite>
         {destinations.map((destination) => (
           <div key={destination.id} className="carousel-data">
             <Item className="img">
@@ -50,7 +50,7 @@ const Destinations = () => {
             </Item>
             <h5 className="dname">{destination.name}</h5>
             <p className="sub1 btn">```````````````````````</p>
-            <p className="sub dabout">{destination.description}</p>
+            <p className="dabout">{destination.description}</p>
           </div>
         ))}
       </Carousel>
--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -6,6 +6,4 @@ export default styled.div`
   justify-content: center;
   align-items: center;
   height: 250px;
-  width: 250px;
-  margin: 1% 5% 0 5%;
 `;
