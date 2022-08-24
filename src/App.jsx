97f0ef142ad3cd1cbbceb8a802a66f96049407e0
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-08-24 20:01:17 +0300

    Fix linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
