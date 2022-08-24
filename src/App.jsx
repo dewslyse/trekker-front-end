6545e46c9fe7af4854e0256c22fa795e7129c1ab
Teshome Kurabachew <dear.teshe@gmail.com>
2022-08-24 21:51:09 +0300

    Fix linters error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
