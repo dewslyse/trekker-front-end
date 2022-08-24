80b5a2b183aa22edd1b17d6784227af20f2f3eeb
buhariaminat <buhariaminat@yahoo.com>
2022-08-24 16:53:44 +0100

    Add route to Reservation page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import RequireAuth from './components/RequireAuth';
+import Reservation from './components/SingleReservations';
 import Reservations from './components/Reservation';
 
 const App = () => {
