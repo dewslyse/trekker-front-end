193e9331eb7fed911391c28f10adc37cc950b771
dewslyse <dewslyse@gmail.com>
2022-08-24 10:19:43 +0000

    Add carousel to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
+// import Sidebar from '../components/Sidebar';
 import Notification from '../components/Notification';
+import HomeCarousel from '../components/HomeCarousel';
 
 const Home = () => {
   const notification = useSelector((state) => state.ui.notification);
 
   return (
     <>
-      <Sidebar />
+      {/* <Sidebar /> */}
       {notification && (<Notification message={notification.message} />)}
+      <HomeCarousel />
       <Outlet />
     </>
   );
