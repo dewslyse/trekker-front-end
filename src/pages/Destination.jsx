54974c0903036a72167f6d29de1753a23cad094d
buhariaminat <buhariaminat@yahoo.com>
2022-08-25 11:06:33 +0100

    Add reserve button to show page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, Link } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { fetchDestinations } from '../store/actions/destinationActions';
 
 const Destination = () => {
   const destinations = useSelector((state) => state.destinations);
-  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDestinations());
+  }, []);
+  const params = useParams();
   return (
-    
-  <>
-    <Sidebar />
-      <div>Destination</div>
-  </>
+    <>
+      <Sidebar />
+      <div>
+        {destinations.map((destination) => (
+          destination.id === parseInt(params.id, 10) && (
+          <div key={destination.id}>
+            <h2>{destination.name}</h2>
+            <p>{destination.city_name}</p>
+            <p>{destination.description}</p>
+            <p>{destination.fee}</p>
+            <img src={destination.image_url} alt={destination.name} />
+            <Link to="/reservations">
+              <button type="button">Reserve</button>
+            </Link>
+          </div>
 
-  )
-  
- 
+          )
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Destination;
