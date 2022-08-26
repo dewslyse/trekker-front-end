108ce64bf5735261387b76285b5fb7d151b4af4c
dewslyse <dewslyse@gmail.com>
2022-08-26 02:51:21 +0000

    Add styles to main page

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -4,37 +4,17 @@ import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import Carousel from 'react-multi-carousel';
-// import Sidebar from '../components/Sidebar';
-// import Item from './Item';
+import Sidebar from '../components/Sidebar';
 import 'react-multi-carousel/lib/styles.css';
-// import '../styles/destinations.scss';
-// import mobilemenu from '../images/mobilemenu.png';
+import { useNavigate } from 'react-router-dom';
 import styles from './Destinations.module.scss';
-
-// const responsive = {
-//   superLargeDesktop: {
-//     breakpoint: { max: 4000, min: 3000 },
-//     items: 5,
-//   },
-//   desktop: {
-//     breakpoint: { max: 3000, min: 1024 },
-//     items: 3,
-//   },
-//   tablet: {
-//     breakpoint: { max: 1024, min: 464 },
-//     items: 2,
-//   },
-//   mobile: {
-//     breakpoint: { max: 464, min: 0 },
-//     items: 1,
-//   },
-// };
 
 const Destinations = () => {
   const destinations = useSelector((state) => state.destinations);
 
-  const { card, cardTop, cardBottom } = styles;
+  const navigate = useNavigate();
+
+  const { card, cardTop, cardBottom, carouselContainer, carouselTitle } = styles;
   const settings = {
     dots: true,
     infinite: false,
@@ -69,64 +49,37 @@ const Destinations = () => {
       },
     ],
   };
+
+  const handleSpecificPage = (id) => {
+    navigate(`/destinations/${id}`);
+  };
+
   return (
-    <Slider {...settings}>
-      {destinations.map((destination) => (
-        <div className={card} key={destination.id}>
-          <div className={cardTop}>
-            <img src={destination.image_url} alt={destination.name} />
-            <h2>{destination.city_name}</h2>
-          </div>
-          <div className={cardBottom}>
-            <h3>{destination.name}</h3>
-            <p>{destination.description}</p>
-          </div>
-        </div>
-      ))}
-    </Slider>
+    <div className="page-container">
+      <Sidebar />
+
+      <div className={carouselContainer}>
+        <h1 className={carouselTitle}>LATEST SAFARIS</h1>
+        <Slider {...settings} >
+          {destinations.map((destination) => (
+            <div>
+              <div className={card} key={destination.id}>
+                <div className={cardTop}>
+                  <img src={destination.image_url} alt={destination.name} />
+                  <h2>{destination.city_name}</h2>
+                </div>
+                <div className={cardBottom}>
+                  <h3>{destination.name}</h3>
+                  <p>{destination.description}</p>
+                  <button type="button" onClick={() => handleSpecificPage(destination.id)}>Learn More</button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
   );
 };
 
 export default Destinations;
-
-/* <h5 className="dname">{destination.name}</h5>
-<p className="sub1 btn">```````````````````````</p>
-<p className="dabout">{destination.description}</p> */
-
-/* <h1 className="heading title">LATEST SAFARIS</h1>
-<p className="sub title">Have a funtastic experience</p>
-<p className="sub1 title">```````````````````````</p>
-<Carousel responsive={responsive} className="carousel" infinite>
-  {destinations.map((destination) => (
-    <div key={destination.id} className={carouselCard}>
-      <Item className="img">
-        <img
-          className="dimage"
-          src={destination.image_url}
-          alt={destination.name}
-        />
-      </Item>
-    </div>
-  ))}
-</Carousel> */
-
-/* <h5 className="dname">{destination.name}</h5>
-<p className="sub1 btn">```````````````````````</p>
-<p className="dabout">{destination.description}</p> */
-
-/* <h1 className="heading title">LATEST SAFARIS</h1>
-<p className="sub title">Have a funtastic experience</p>
-<p className="sub1 title">```````````````````````</p>
-<Carousel responsive={responsive} className="carousel" infinite>
-  {destinations.map((destination) => (
-    <div key={destination.id} className={carouselCard}>
-      <Item className="img">
-        <img
-          className="dimage"
-          src={destination.image_url}
-          alt={destination.name}
-        />
-      </Item>
-    </div>
-  ))}
-</Carousel> */
